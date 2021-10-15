830d3e7c4f77ed39eb277228ca2eb6c8897995ce
marketsupreme <mike855@outlook.com>
2021-10-15 08:20:59 -0500

    hourly commit

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-				10/15/21 @ 08:09:10 
+				10/15/21 @ 08:20:57 
 
-				Value: $164834.23
+				Value: $166163.48
 
 
 
-BTC Price = $59500.17
-\ ETH Price = $3774.63
+BTC Price = $59961.66
+\ ETH Price = $3790.35
+\ DOGE Price = $0.23
 
 
 BTC Holdings = 1.06BTC
 
- ETH holdings = 26.960005962ETH 
+ ETH holdings = 26.960005962ETH
+
+ DOGE Holdings = 1809.826DOGE
 
--- mcp.py
+++ mcp.py
@@ -10,27 +10,25 @@ from openpyxl import load_workbook
 
 def getValue():
 
-    prices = get('https://api.nomics.com/v1/currencies/ticker?key=ea386addbac03f4bb67ceb1f333a8d0a&ids=BTC,ETH&interval=1d&convert=USD&per-page=100&page=1')
+    prices = get('https://api.nomics.com/v1/currencies/ticker?key=ea386addbac03f4bb67ceb1f333a8d0a&ids=BTC,ETH,DOGE&interval=1d&convert=USD&per-page=100&page=1')
     data = prices.json()
 
     bit_price = round(float(data[0]['price']),2)
     eth_price = round(float(data[1]['price']),2)
+    doge_price =round(float(data[2]['price']),2)
 
     e = get('https://api.ethplorer.io/getAddressInfo/0xed8b4b3ba4fd5a175613859cab6ab8f010276a3a?apiKey=freekey')
     data = e.json()
 
     eth_holdings = data['ETH']['balance']
-
-    # b = get('https://blockchain.info/balance?active=16MuqwYTRT1qTFmwF5WsgfsrA9rE3UmPQN')
-    # data = b.json()
-
-    # bit_holdings1 = data['16MuqwYTRT1qTFmwF5WsgfsrA9rE3UmPQN']['final_balance']
     bit_holdings = 1.06
+    doge_holdings = 1809.826
 
     eth_value = eth_holdings*eth_price
     bit_value = bit_holdings*bit_price
+    doge_value = doge_price*doge_holdings
 
-    return [bit_price, bit_value, bit_holdings, eth_price, eth_value, eth_holdings]
+    return [bit_price, bit_value, bit_holdings, eth_price, eth_value, eth_holdings, doge_price, doge_holdings, doge_value]
 
 def main():
 
@@ -40,14 +38,14 @@ def main():
 
     coin_list = getValue()
 
-    vals = round(coin_list[1]+coin_list[4],2)
+    vals = round(coin_list[1]+coin_list[4]+coin_list[8],2)
 
     file.write(f'\t\t\t\t{today} @ {time} \n\n')
     file.write(f'\t\t\t\tValue: ${vals}\n\n\n\n')
-    file.write(f'BTC Price = ${coin_list[0]}\n\ ETH Price = ${coin_list[3]}\n\n\n')
-    file.write(f'BTC Holdings = {coin_list[2]}BTC\n\n ETH holdings = {coin_list[5]}ETH \n\n')
+    file.write(f'BTC Price = ${coin_list[0]}\n\ ETH Price = ${coin_list[3]}\n\ DOGE Price = ${coin_list[6]}\n\n\n')
+    file.write(f'BTC Holdings = {coin_list[2]}BTC\n\n ETH holdings = {coin_list[5]}ETH\n\n DOGE Holdings = {coin_list[7]}DOGE\n\n')
 
-    df = pd.DataFrame({'Date':[str(today)],'Value':[str(vals)]})
+    df = pd.DataFrame({'Date':[str(today)],'Value':[vals]})
     
     wb = load_workbook(filename = 'value.xlsx')
     ws = wb['Sheet']
